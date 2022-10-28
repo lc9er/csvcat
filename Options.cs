361f83a4a1c207310442b5e71039377e31b1d406
LC9er <liquidcloud9@gmail.com>
2022-10-28 00:22:50 -0400

    csvcat util

--- Options.cs
+++ Options.cs
@@ -14,8 +14,8 @@ namespace csvcat
         [Option('t', "tail", Default = false, HelpText = "Tail csv file")]
         public bool Tail { get; set; }
 
-        //[Option('s', "sample", Default = false, HelpText = "Random sample of csv file")]
-        //public bool Sample { get; set; }
+        [Option('d', "delimiter", Default = ',', HelpText = "Delimiting character. Default = ','")]
+        public char Delimiter { get; set; }
 
         [Usage(ApplicationAlias = "csvcat")]
         public static IEnumerable<Example> Examples
@@ -23,9 +23,8 @@ namespace csvcat
             get
             {
                 yield return new Example("cat csv file", new Options { Filename = "file.csv", Lines = 20});
-                yield return new Example("tail csv file", new Options { Filename = "file.csv", Lines = 30, Tail = true });
+                yield return new Example("tail csv file", new Options { Filename = "file.csv", Lines = 30, Tail = true, Delimiter = '|' });
             }
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using Spectre.Console;
-using Sylvan.Data.Csv;
+﻿using CommandLine;
+using CommandLine.Text;
+using Spectre.Console;
 
 namespace csvcat
 {
@@ -7,9 +8,36 @@ namespace csvcat
     {
         static void Main(string[] args)
         {
-
+            var parser = new CommandLine.Parser(with => with.HelpWriter = null);
+            var parserResults = parser.ParseArguments<Options>(args);
+            parserResults
+                .WithParsed<Options>(opts =>
+                    {
+                        Run(opts.Filename, opts.Lines, opts.Tail, opts.Delimiter);
+                    })
+                .WithNotParsed(errs => DisplayHelp(parserResults, errs));
         }
 
+        static void Run(string fileName, int lines, bool tail, char delimiter)
+        {
+            var catLines = new ParseCsv(fileName, lines, tail, delimiter);
+            catLines.ReadCsvFile();
 
+            // display result table
+            OutputTable table = new();
+            OutputTable.PrintTable(catLines);
+        }
+
+        static void DisplayHelp<T>(ParserResult<T> result, IEnumerable<Error> errs)
+        {
+            var helpText = HelpText.AutoBuild(result, h =>
+            {
+                h.AdditionalNewLineAfterOption = false;
+                h.Heading = "csvcat 0.0.1";
+                h.Copyright = "Copyright (c) 2022 lc9er";
+                return HelpText.DefaultParsingErrorsHandler(result, h);
+            }, e => e);
+            Console.WriteLine(helpText);
+        }
     }
 }
